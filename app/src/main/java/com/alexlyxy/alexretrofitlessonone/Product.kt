b0a40459a66d04b526c466777b7b52b9790f060e
Oleksandr Lysenko <alexlyxy@gmail.com>
2023-10-19 00:15:35 +0300

    Initial commit

---
+++ app/src/main/java/com/alexlyxy/alexretrofitlessonone/Product.kt
@@ -0,0 +1,16 @@
+package com.alexlyxy.alexretrofitlessonone
+
+data class Product(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val price: Int,
+    val discountPercentage: Float,
+    val rating: Float,
+    val stock: Int,
+    val brand: String,
+    val category: String,
+    val thumbnail: String,
+    val images: List<String>
+)
+
